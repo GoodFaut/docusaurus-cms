91f1938d9738637df7d08ff6058b49f96f4a0ab8
GoodFaut <70525789+GoodFaut@users.noreply.github.com>
2021-04-11 16:00:17 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,7 +92,7 @@ module.exports = {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/GoodFaut/docusaurus-cms/edit/master/website/',
+            'https://github.com/GoodFaut/docusaurus-cms/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
