b77acd77677d8d66b82552666a58a91ac5b93feb
GoodFaut <70525789+GoodFaut@users.noreply.github.com>
2021-04-11 15:58:26 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,13 +86,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/GoodFaut/docusaurus-cms/edit/master/website/',
+            'https://github.com/GoodFaut/docusaurus-cms/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/GoodFaut/docusaurus-cms/edit/master/website/blog/',
+            'https://github.com/GoodFaut/docusaurus-cms/edit/master/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
